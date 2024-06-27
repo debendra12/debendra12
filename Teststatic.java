27a4286fa8d070834ebeafaf6d1e28bb6adc008f
debendra12 <debendrapandey3@gmail.com>
2024-06-27 21:01:34 +0545

    Create Teststatic.java

---
+++ Teststatic.java
@@ -0,0 +1,20 @@
+public class TestStatic 
+{
+    public static void main(String[] args) {
+
+		
+		TestStatic.myStaticMethod();
+		TestStatic.allFreeMethod();
+
+	}
+
+	private static void allFreeMethod() {
+		System.out.println("i am another method");
+	}
+
+	private static void myStaticMethod() {
+		System.out.println("I am static method");
+
+	}
+
+}
