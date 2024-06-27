64eac73d124af161481ea8a5bb57ef5d8e5d415d
debendra12 <debendrapandey3@gmail.com>
2024-06-27 20:56:32 +0545

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,1 +1,37 @@
+package sahin.three.lll;
+
+import java.awt.Frame;
+import java.awt.Lable;
+import java.awt.TextField;
+import java.awt.Button;
+
+public class LOGIN
+{
+    public static void main (String[] args)
+{
+    Frame frame = new Frame("LOGIN PAGE");
+
+    Label lbl1 = new Label("Username");
+    setBounds.lbl1(200,200,100,50);
+    frame.add(lbl1);
+
+    TextField txt1 = new TextField();
+    setBounds.txt1(250,200,300,50);
+    frame.add(txt1);
+
+    Label lbl2 = new Label("password");
+    setBounds.lbl2(200,300,100,50);
+    frame.add(lbl2);
+
+    TextField txt2 = new TextField;
+    setBounds.txt2(250,300,300,50);
+    frame.add(txt2);
+
+    frame.setSize(400,400);
+    frame.setLayout(null);
+    frame.setVisible(true);
+
+}
+
+}
 
